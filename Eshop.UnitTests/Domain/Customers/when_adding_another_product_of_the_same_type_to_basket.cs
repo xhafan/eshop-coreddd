96e368647c38a0c34713a4d9f35e862b301f2169
xhafan <xhafan@a201011f-f758-6885-1f49-957facf236c9>
2013-10-30 22:25:31 +0000

    Removed .AsSet() usages from unit tests UnitTests tested in Detroit style, allowing multiple methods calls on a SUT for a test

--- Eshop.UnitTests/Domain/Customers/when_adding_another_product_of_the_same_type_to_basket.cs
+++ Eshop.UnitTests/Domain/Customers/when_adding_another_product_of_the_same_type_to_basket.cs
@@ -3,36 +3,30 @@ using CoreTest;
 using Eshop.Domain;
 using NUnit.Framework;
 using Shouldly;
-using Rhino.Mocks;
 
 namespace Eshop.UnitTests.Domain.Customers
 {
     [TestFixture]
     public class when_adding_another_product_of_the_same_type_to_basket : BaseTest
     {
-        private const int Count = 23;
         private Customer _customer;
         private Product _product;
-        private BasketItem _basketItem;
 
         [SetUp]
         public void Context()
         {
             _product = Stub<Product>();
             _customer = new Customer();
-            _basketItem = Mock<BasketItem>()
-                .Stubs(x => x.Product).Returns(_product)
-                .Stubs(x => x.Count).Returns(1);
-            _customer.BasketItems.AsSet().Add(_basketItem);
+            _customer.AddProductToBasket(_product, 1);
 
-            _customer.AddProductToBasket(_product, Count);
+            _customer.AddProductToBasket(_product, 1);
         }
 
         [Test]
         public void product_count_is_updated()
         {
             _customer.BasketItems.Count().ShouldBe(1);
-            _basketItem.AssertWasCalled(x => x.AddCount(Count));
+            _customer.BasketItems.First().Count.ShouldBe(2);
         }
     }
 }
