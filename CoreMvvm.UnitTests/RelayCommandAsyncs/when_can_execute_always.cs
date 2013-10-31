43d800bc58a7c98e7208dd6741fd6821f5fd0d19
xhafan <xhafan@a201011f-f758-6885-1f49-957facf236c9>
2013-10-31 00:22:28 +0000

    CoreMvvm unit tests added

---
+++ CoreMvvm.UnitTests/RelayCommandAsyncs/when_can_execute_always.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+
+namespace CoreMvvm.UnitTests.RelayCommandAsyncs
+{
+    [TestFixture]
+    public class when_can_execute_always
+    {
+        private RelayCommandAsync<object> _relayCommand;
+        private bool _canExecute;
+
+        private async Task ExecuteAsync(object obj) {}
+
+        [SetUp]
+        public void Context()
+        {
+            _relayCommand = new RelayCommandAsync<object>(async obj => await ExecuteAsync(obj));
+
+            _canExecute = _relayCommand.CanExecute(null);
+        }
+
+        [Test]
+        public void can_always_execute()
+        {
+            _canExecute.ShouldBe(true);
+        }
+    }
+}
