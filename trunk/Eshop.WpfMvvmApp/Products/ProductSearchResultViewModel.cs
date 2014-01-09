c1316b076751a0ba5b644853824b93c736c66c07
xhafan <xhafan@a201011f-f758-6885-1f49-957facf236c9>
2014-01-09 22:07:33 +0000

    Product search control always visible, switching between ProductSearchResultView and ProductDetailsView below the search

---
+++ trunk/Eshop.WpfMvvmApp/Products/ProductSearchResultViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CoreMvvm;
+using CoreUtils.Extensions;
+using Eshop.Dtos;
+
+namespace Eshop.WpfMvvmApp.Products
+{
+    public class ProductSearchResultViewModel : NotifyingObject
+    {
+        private readonly ObservableCollection<ProductViewModel> _products = new ObservableCollection<ProductViewModel>();
+        
+        private readonly RelayCommandAsync<int> _selectProductCommand;
+
+        public ProductSearchResultViewModel()
+        {
+            _selectProductCommand = new RelayCommandAsync<int>(async x => await SelectProduct(x), CanSelectProductExecute);
+        }
+
+        public ObservableCollection<ProductViewModel> Products { get { return _products; } }
+
+        public ICommand SelectProductCommand { get { return _selectProductCommand; } }
+
+        public Task PopulateSearchResult(IEnumerable<ProductSummaryDto> products)
+        {
+            Products.Clear();
+            products.Each(x => Products.Add(new ProductViewModel(x.Id, x.Name)));
+            return TaskEx.FromResult(0); // todo: extract this to TaskEx.CompletedTask(); or sth
+        }
+        
+        public bool CanSelectProductExecute(int productId)
+        {
+            return true;
+        }
+
+        public delegate Task OnProductSelectedHandler(int productId);
+        public event OnProductSelectedHandler OnProductSelected;
+
+        public async Task SelectProduct(int productId)
+        {
+            await OnProductSelected(productId);
+        }
+    }
+}
--- trunk/Eshop.WpfMvvmApp/Products/ProductSearchViewModel.cs
+++ trunk/Eshop.WpfMvvmApp/Products/ProductSearchViewModel.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using CoreMvvm;
-using CoreUtils.Extensions;
+using Eshop.Dtos;
 using Eshop.WpfMvvmApp.ControllerClients;
 
 namespace Eshop.WpfMvvmApp.Products
@@ -10,24 +10,19 @@ namespace Eshop.WpfMvvmApp.Products
     public class ProductSearchViewModel : NotifyingObject
     {
         private readonly IProductControllerClient _productControllerClient;
-        private readonly ObservableCollection<ProductViewModel> _products = new ObservableCollection<ProductViewModel>();
 
         private readonly RelayCommandAsync<string> _searchProductsCommand;
-        private readonly RelayCommandAsync<int> _selectProductCommand;
         
         public ProductSearchViewModel(IProductControllerClient productControllerClient)
         {
             _productControllerClient = productControllerClient;
         
             _searchProductsCommand = new RelayCommandAsync<string>(async x => await SearchProducts(x), CanSearchProductsExecute);
-            _selectProductCommand = new RelayCommandAsync<int>(async x => await SelectProduct(x), CanSelectProductExecute);
         }
 
-        public ObservableCollection<ProductViewModel> Products { get { return _products; } }
         public string SearchText { get; set; }
 
         public ICommand SearchProductsCommand { get { return _searchProductsCommand; } }
-        public ICommand SelectProductCommand { get { return _selectProductCommand; } }
 
         public bool IsBusy { get; private set; } // todo: refactor this into some base view model
         public bool IsNotBusy { get { return !IsBusy; } }
@@ -37,29 +32,17 @@ namespace Eshop.WpfMvvmApp.Products
             return true;
         }
 
+        public delegate Task OnProductSearchedHandler(IEnumerable<ProductSummaryDto> products);
+        public event OnProductSearchedHandler OnProductSearched;
+       
         public async Task SearchProducts(string searchText)
         {
             IsBusy = true;
 
             var products = await _productControllerClient.GetAsync(searchText);
-            Products.Clear();
-            products.Each(x => Products.Add(new ProductViewModel(x.Id, x.Name)));
+            await OnProductSearched(products);
 
             IsBusy = false;
         }
-
-        public bool CanSelectProductExecute(int productId)
-        {
-            return true;
-        }
-
-        public delegate Task OnProductSelectedHandler(int productId);
-        public event OnProductSelectedHandler OnProductSelected;
-
-        public async Task SelectProduct(int productId)
-        {
-            await OnProductSelected(productId);
-        }
-
     }
 }
