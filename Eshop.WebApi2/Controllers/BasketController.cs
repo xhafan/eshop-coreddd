8478d11147f7595063887901782eb53fe6d69c6d
xhafan <xhafan@a201011f-f758-6885-1f49-957facf236c9>
2014-03-23 21:52:04 +0000

    BasketController.AddProductToBasket implemented

--- Eshop.WebApi2/Controllers/BasketController.cs
+++ Eshop.WebApi2/Controllers/BasketController.cs
@@ -1,19 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Http;
 using CoreDdd.Commands;
 using CoreDdd.Queries;
+using Eshop.Commands;
 using Eshop.Dtos;
 
 namespace Eshop.WebApi2.Controllers
 {
     public class BasketController : AuthenticatedController
     {
+        private readonly ICommandExecutor _commandExecutor;
+
         public BasketController(IQueryExecutor queryExecutor, ICommandExecutor commandExecutor)
         {
+            _commandExecutor = commandExecutor;
         }
 
         public void AddProductToBasket([FromBody]int productId, int quantity)
         {
+            _commandExecutor.CommandExecuted += SetGeneratedCustomerIdToSessionContext;
+            _commandExecutor.Execute(new AddProductToBasketCommand
+                {
+                    CustomerId = SessionContext != null ? SessionContext.CustomerId : default(int),
+                    ProductId = productId,
+                    Quantity = quantity
+                });
+        }
+
+        private void SetGeneratedCustomerIdToSessionContext(object sender, CommandExecutedArgs commandExecutedArgs)
+        {
+            var generatedCustomerId = (int)commandExecutedArgs.Args;
+            SessionContext = new SessionContext { CustomerId = generatedCustomerId };
         }
 
         public IEnumerable<BasketItemDto> GetBasketItems()
