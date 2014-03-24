7aec2d88633e26ad3183a6a5e30045c0a9f28e21
xhafan@gmail.com <xhafan@gmail.com@a201011f-f758-6885-1f49-957facf236c9>
2014-03-24 21:44:38 +0000

    project Eshop.WebApiTemp renamed to Eshop.WebApi

---
+++ trunk/Eshop.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using CoreDdd.Queries;
+using Eshop.Dtos;
+using Eshop.Queries;
+
+namespace Eshop.WebApi.Controllers
+{
+    public class ProductController : AuthenticatedController
+    {
+        private readonly IQueryExecutor _queryExecutor;
+
+        public ProductController(IQueryExecutor queryExecutor)
+        {
+            _queryExecutor = queryExecutor;
+        }
+
+        [HttpGet]
+        public IEnumerable<ProductSummaryDto> SearchProducts(string searchedText = "")
+        {
+            var productDtos = _queryExecutor.Execute<ProductsQuery, ProductSummaryDto>(new ProductsQuery { SearchText = searchedText });
+            return productDtos.ToArray();
+        }
+
+        public ProductDto GetProduct(int productId)
+        {
+            var productDtos = _queryExecutor.Execute<ProductDetailsQuery, ProductDto>(new ProductDetailsQuery { ProductId = productId });
+            return productDtos.Single();            
+        }     
+    }
+}
