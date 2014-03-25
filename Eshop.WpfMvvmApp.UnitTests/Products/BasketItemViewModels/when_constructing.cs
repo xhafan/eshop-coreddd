0b3c625a44e12f0fd91dc42f98ffa0e7085f5bb7
xhafan <xhafan@a201011f-f758-6885-1f49-957facf236c9>
2014-03-25 22:04:55 +0000

    BasketItemViewModel and BasketViewModel tests added

---
+++ Eshop.WpfMvvmApp.UnitTests/Products/BasketItemViewModels/when_constructing.cs
@@ -0,0 +1,48 @@
+﻿using CoreTest;
+using Eshop.Dtos;
+using Eshop.WpfMvvmApp.Products;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Eshop.WpfMvvmApp.UnitTests.Products.BasketItemViewModels
+{
+    [TestFixture]
+    public class when_constructing : BaseTest
+    {
+        private const int ProductId = 23;
+        private const string ProductName = "product name";
+        private const int Quantity = 34;
+        private BasketItemViewModel _viewModel;
+
+        [SetUp]
+        public void Context()
+        {
+            var basketItemDto = new BasketItemDto
+                {
+                    ProductId = ProductId,
+                    ProductName = ProductName,
+                    Count = Quantity
+                };
+
+            _viewModel = new BasketItemViewModel(basketItemDto);
+        }
+
+        [Test]
+        public void product_id_is_correct()
+        {
+            _viewModel.ProductId.ShouldBe(ProductId);
+        }
+
+        [Test]
+        public void product_name_is_correct()
+        {
+            _viewModel.ProductName.ShouldBe(ProductName);
+        }
+
+        [Test]
+        public void quantity_is_correct()
+        {
+            _viewModel.Quantity.ShouldBe(Quantity);
+        }
+    }
+}
