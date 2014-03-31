454d606c2b7cc571293a0d61426df2a91376867c
xhafan <xhafan@a201011f-f758-6885-1f49-957facf236c9>
2014-03-31 19:53:41 +0000

    ProductDetailsViewModel tests added

---
+++ trunk/Eshop.WpfMvvmApp.UnitTests/Products/ProductDetailsViewModels/when_executing_add_to_basket_command.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Eshop.WpfMvvmApp.UnitTests.Products.ProductDetailsViewModels
+{
+    [TestFixture]
+    public class when_executing_add_to_basket_command : ProductDetailsViewModelWithLoadedProductSetup
+    {
+        [SetUp]
+        public override void Context()
+        {
+            base.Context();
+            BasketControllerClient.Expect(x => x.AddProductToBasketAsync(ProductId, Quantity)).Return(TaskEx.FromResult(0));
+
+            ViewModel.AddToBasketCommand.Execute(Quantity);
+        }
+
+        [Test]
+        public void execute_adds_product_to_basket()
+        {
+            BasketControllerClient.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void execute_notifies_that_product_is_added_to_basket()
+        {
+            ProductAddedToBasket.AssertWasCalled(x => x.ProductAddedToBasket());
+        }
+    }
+}
