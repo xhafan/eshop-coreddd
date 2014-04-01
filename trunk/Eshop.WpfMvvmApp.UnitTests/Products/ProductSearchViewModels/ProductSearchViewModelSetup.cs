92743d51759c782a36fe8a6f9bf1f3ef7370d2bb
xhafan <xhafan@a201011f-f758-6885-1f49-957facf236c9>
2014-04-01 19:41:13 +0000

    ProductSearchViewModel tests added

---
+++ trunk/Eshop.WpfMvvmApp.UnitTests/Products/ProductSearchViewModels/ProductSearchViewModelSetup.cs
@@ -0,0 +1,22 @@
+﻿using CoreTest;
+using Eshop.WpfMvvmApp.ControllerClients;
+using Eshop.WpfMvvmApp.Products;
+using NUnit.Framework;
+
+namespace Eshop.WpfMvvmApp.UnitTests.Products.ProductSearchViewModels
+{
+    public abstract class ProductSearchViewModelSetup : BaseTest
+    {
+        protected IProductSearched ProductSearched;
+        protected IProductControllerClient ProductControllerClient;
+        protected ProductSearchViewModel ViewModel;
+
+        [SetUp]
+        public virtual void Context()
+        {
+            ProductControllerClient = Stub<IProductControllerClient>();
+            ProductSearched = Mock<IProductSearched>();
+            ViewModel = new ProductSearchViewModel(ProductControllerClient, ProductSearched);            
+        }
+    }
+}
