693d5025091a48dcdae95f8be00fc889174d89b2
xhafan <xhafan@a201011f-f758-6885-1f49-957facf236c9>
2014-04-23 18:59:31 +0000

    Basket item quantity textbox restricted to numbers only

---
+++ trunk/Eshop.WpfMvvmApp/Behaviours/PositiveNumbersTextBoxBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace Eshop.WpfMvvmApp.Behaviours
+{
+    public class PositiveNumbersTextBoxBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewTextInput += OnPreviewTextInput;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PreviewTextInput -= OnPreviewTextInput;
+        }
+
+        private void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsValid(e.Text);
+        }
+
+        private bool IsValid(string text)
+        {
+            return Regex.IsMatch(text, "^[0-9]*$");
+        }
+    }
+}
