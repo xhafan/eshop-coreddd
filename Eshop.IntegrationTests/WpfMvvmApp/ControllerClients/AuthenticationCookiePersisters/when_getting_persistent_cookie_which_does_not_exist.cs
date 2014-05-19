916d272e49ecc77b4acc7818e77c6f62ca15206a
xhafan <xhafan@a201011f-f758-6885-1f49-957facf236c9>
2014-05-19 21:37:09 +0000

    AuthenticationCookiePersister implemented

---
+++ Eshop.IntegrationTests/WpfMvvmApp/ControllerClients/AuthenticationCookiePersisters/when_getting_persistent_cookie_which_does_not_exist.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Net;
+using CoreTest;
+using Eshop.WpfMvvmApp.ControllerClients;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Eshop.IntegrationTests.WpfMvvmApp.ControllerClients.AuthenticationCookiePersisters
+{
+    [TestFixture]
+    public class when_getting_persistent_cookie_which_does_not_exist : BaseTest
+    {
+        private Cookie _cookie;
+
+        [SetUp]
+        public void Context()
+        {
+            File.Delete(AuthenticationCookiePersister.PersistentCookiePath);
+
+            var authenticationCookiePersister = new AuthenticationCookiePersister();
+            _cookie = authenticationCookiePersister.GetPersistentAuthenticationCookie();
+        }
+
+        [Test]
+        public void cookie_is_null()
+        {
+            _cookie.ShouldBe(null);
+        }
+    }
+}
---
+++ Eshop.IntegrationTests/WpfMvvmApp/ControllerClients/AuthenticationCookiePersisters/when_setting_cookie_and_getting_it_again_in_another_persister_instance.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Net;
+using System.Runtime.Serialization.Formatters.Binary;
+using CoreTest;
+using Eshop.WpfMvvmApp.ControllerClients;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Eshop.IntegrationTests.WpfMvvmApp.ControllerClients.AuthenticationCookiePersisters
+{
+    [TestFixture]
+    public class when_setting_cookie_and_getting_it_again_in_another_persister_instance : BaseTest
+    {
+        private const string CookieName = "cookie_name";
+        private const string CookieValue = "cookie value";
+        private Cookie _retrievedCookie;
+        private Cookie _savedCookie;
+
+        [SetUp]
+        public void Context()
+        {
+            var authenticationCookiePersister = new AuthenticationCookiePersister();
+            _savedCookie = new Cookie(CookieName, CookieValue);
+            authenticationCookiePersister.SetPersistentAuthenticationCookie(_savedCookie);
+
+            authenticationCookiePersister = new AuthenticationCookiePersister();
+            _retrievedCookie = authenticationCookiePersister.GetPersistentAuthenticationCookie();
+        }
+
+        [Test]
+        public void cookie_is_correct()
+        {
+            _retrievedCookie.Name.ShouldBe(CookieName);
+            _retrievedCookie.Value.ShouldBe(CookieValue);
+        }
+
+        [Test]
+        public void saved_cookie_is_the_same_as_retrieved_cookie()
+        {
+            GetSerializedBytes(_savedCookie).ShouldBe(GetSerializedBytes(_retrievedCookie));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            File.Delete(AuthenticationCookiePersister.PersistentCookiePath);
+        }
+
+        private byte[] GetSerializedBytes(Cookie cookie)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(ms, cookie);
+                return ms.ToArray();
+            }            
+        }
+    }
+}
