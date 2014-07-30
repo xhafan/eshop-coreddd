986e9bbcd27d346d547953fd5e2eaa7431f65148
xhafan <xhafan@a201011f-f758-6885-1f49-957facf236c9>
2014-07-30 19:16:28 +0000

    Added NugetPackages for CoreMvvm and CoreWebApi libraries

--- NugetPackages/create_nuget_packages.bat
+++ NugetPackages/create_nuget_packages.bat
@@ -1,4 +1,10 @@
-cd CoreWebApiClient
+cd CoreMvvm
+..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
+
+cd ..\CoreWebApi
+..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
+
+cd ..\CoreWebApiClient
 ..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
 
 cd ..\CoreWebApiClient.Generator
